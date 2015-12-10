cf718616c8b550806525aff7e294b54d40637141
Arthur Dexter <adexter@dexman.net>
2015-12-10 17:29:16 -0600

    Import FastCGI Developer's Kit source.

---
+++ SwiftFCGI/SwiftFCGI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFCGI.h
+//  SwiftFCGI
+//
+//  Created by Arthur Dexter on 12/10/15.
+//  Copyright © 2015 Arthur Dexter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFCGI.
+FOUNDATION_EXPORT double SwiftFCGIVersionNumber;
+
+//! Project version string for SwiftFCGI.
+FOUNDATION_EXPORT const unsigned char SwiftFCGIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFCGI/PublicHeader.h>
+
+
---
+++ SwiftFCGITests/SwiftFCGITests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftFCGITests.swift
+//  SwiftFCGITests
+//
+//  Created by Arthur Dexter on 12/10/15.
+//  Copyright © 2015 Arthur Dexter. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftFCGI
+
+class SwiftFCGITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
